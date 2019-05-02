0721d11e05f4f06c46f8773eefab3601514b88fb
joyshao37 <102306001@nccu.edu.tw>
2019-05-02 23:46:31 +0800

    feat: nav bar

--- index.php
+++ index.php
@@ -1,33 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>BtsTube</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-	<script src="assets/js/commonAction.js"></script>
-</head>
-<body>
-	<div id="pageContainer">
-
-		<!--navigation bar-->
-		<div id="mastHeadContainer">
-			<button class="navShowHide">CLick</button>
-		</div>
-
-		<div id="sideNavContainer" style="display: none;">
-
-		</div>
-
-		<div id="mainSectionContainer">
-			
-			<div id="mainContentContainer">
+<?php require_once("includes/header.php"); ?>
+				
 				Hello everyone!
-			</div>
-			
-		</div>
-	</div>
-</body>
-</html>
+
+<?php require_once("includes/footer.php"); ?>
