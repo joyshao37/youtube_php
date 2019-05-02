1a3274b970db24ddbb3851c8daf4f05a759de50a
joyshao37 <102306001@nccu.edu.tw>
2019-05-02 23:44:29 +0800

    feat: header & footer & UploadFile Class

---
+++ includes/classes/VideoDetailsFormProvider.php
@@ -0,0 +1,39 @@
+<?php
+Class VideoDetailsFormProvider{
+
+
+	public function createUploadForm(){
+		$fileInput = $this->createFileInput();
+		$titleInput = $this->createTitleInput();
+		$descriptionInput = $this->createDescriptionInput();
+		return "<form action='processing'.php method='POST'>
+					$fileInput
+					$titleInput
+					$descriptionInput
+				</form>";
+	}
+
+	private function createFileInput(){
+		return "<div class='form-group'>
+				    <input type='file' class='form-control-file' name='fileInput' id='exampleFormControlFile1' required>
+			  	</div>";
+	}
+
+	private function createTitleInput(){
+		return "<div class='form-group'>
+					<input class='form-control' type='text' placeholder='Title' name='titleInput'>
+				</div>";
+	}
+
+		private function createDescriptionInput(){
+		return "<div class='form-group'>
+    				<textarea class='form-control' placeholder='Description' name='descriptionInput' rows='3'></textarea>
+  				</div>";
+	}
+
+
+
+}
+
+
+?>
